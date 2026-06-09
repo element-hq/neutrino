6350ffe748ec013e79b8c4de83304fac130f2300
Skye Elliot <actuallyori@gmail.com>
2026-06-09 10:13:31 +0000

    refactor(main): entrypoint takes a built Config; re-export Config

--- crates/neutrino/src/main.rs
+++ crates/neutrino/src/main.rs
@@ -3,5 +3,5 @@
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    neutrino_main::entrypoint().await
+    neutrino_main::entrypoint(neutrino_main::Config::from_env()).await
 }
